c6879a6134e2738767ccdd2cf8e198b7c44c8e2c
Manukyan Artak <artakmanukyan@freedomdebtrelief.com>
2022-08-12 20:46:30 +0400

    About book component

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,8 +1,11 @@
 import MainLayout from '../src/components/layout/Main'
+import HeroBanner from "../src/components/shared/HeroBanner";
+import PageConstants from "../src/const";
 
 export default function About() {
     return (
         <MainLayout>
+            <HeroBanner text={PageConstants.aboutPage.heroBannerText}/>
             <h1>About</h1>
         </MainLayout>
     )
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,20 +1,13 @@
 import MainLayout from '../src/components/layout/Main'
 import HeroBanner from "../src/components/shared/HeroBanner";
 import PageConstants from "../src/const";
+import AboutBook from "../src/components/shared/AboutBook";
 
 export default function Home() {
   return (
       <MainLayout>
           <HeroBanner text={PageConstants.homePage.heroBannerText}/>
-          <div>
-              <h1>test</h1>
-              <h1>test</h1>
-              <h1>test</h1>
-              <h1>test</h1>
-              <h1>test</h1>
-              <h1>test</h1>
-              <h1>test</h1>
-          </div>
+          <AboutBook/>
       </MainLayout>
 
   )
